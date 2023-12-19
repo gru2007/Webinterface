fc24b78dcfd5f4b44025787d1eee383f5329d70d
Gru <ruslru376@gmail.com>
2023-12-19 10:25:56 +1000

    bug

--- Backend/src/main/java/de/presti/ree6/backend/utils/data/container/RecordContainer.java
+++ Backend/src/main/java/de/presti/ree6/backend/utils/data/container/RecordContainer.java
@@ -17,14 +17,14 @@ public class RecordContainer {
 
     String creationTime;
     String guildId;
-    ChannelContainer voiceChannel;
+    String voiceChannel;
     UserContainer creator;
 
     public RecordContainer(Recording recording) {
         ///this.data = Base64.encodeBase64String(recording.getRecording());
         this.creationTime = String.valueOf(recording.getCreation());
         this.guildId = recording.getGuildId();
-        this.voiceChannel = new ChannelContainer(BotWorker.getShardManager().getChannelById(StandardGuildMessageChannel.class, recording.getVoiceId()));
+        this.voiceChannel = recording.getVoiceId();
         this.creator = new UserContainer(BotWorker.getShardManager().retrieveUserById(recording.getCreatorId()).complete());
     }
 }
